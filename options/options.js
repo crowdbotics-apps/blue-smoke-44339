fe4670730ed9a5a461e412c73555cfe01496db0e
Crowdbotics <team@crowdbotics.com>
2023-11-04 21:21:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "blue-smoke-44339",
+  url: "https://blue-smoke-44339.botics.co",
+  api: "https://blue-smoke-44339.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
